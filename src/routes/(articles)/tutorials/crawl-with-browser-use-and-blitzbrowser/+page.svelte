337701f4f12531317f2195b8b060288aeb327a47
Samuel M-Lavoie <samuel.m.lavoie@gmail.com>
2025-06-09 09:50:45 -0400

    Updated browser-use doc

--- src/routes/(articles)/tutorials/crawl-with-browser-use-and-blitzbrowser/+page.svelte
+++ src/routes/(articles)/tutorials/crawl-with-browser-use-and-blitzbrowser/+page.svelte
@@ -51,7 +51,10 @@
 	In this example, we are using the GPT-4o model of OpenAI. You need to
 	<a href="https://openai.com/index/hello-gpt-4o/" target="_blank">create an OpenAI account</a> to
 	run the code. Once you have an OpenAI API key, you have to set the environment variable
-	<code>OPENAI_SECRET_KEY</code>.
+	<code>OPENAI_SECRET_KEY</code>. If you want to use another LLM, you can follow their
+	<a href="https://docs.browser-use.com/customize/supported-models" target="_blank">
+		documentation here
+	</a>.
 </p>
 <h3>Scrape pricing plans of BlitzBrowser example</h3>
 <pre class="language-typescript overflow-hidden rounded">{@html browserUseTutorial}</pre>
